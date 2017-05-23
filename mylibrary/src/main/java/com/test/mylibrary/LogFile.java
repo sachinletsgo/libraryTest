ce0edfe4f41c0769ffea937cc6f5a4229f4f79e4
Sachin Tyagi <SachinTyagi@Sachins-MacBook-Pro.local>
2017-05-23 15:56:47 +0530

    firat library

---
+++ mylibrary/src/main/java/com/test/mylibrary/LogFile.java
@@ -0,0 +1,15 @@
+package com.test.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by SachinTyagi on 5/23/17.
+ */
+
+public class LogFile {
+
+    public void write(String msg){
+
+        Log.d("LIBRARY", msg);
+    }
+}
